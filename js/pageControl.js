531ada51e17663e82253e96aa0e483c16259868a
bee2side <bee2side@gmail.com>
2017-03-04 18:05:32 +0900

    +

--- js/pageControl.js
+++ js/pageControl.js
@@ -1,37 +1,36 @@
 $(document).ready(function(){
-	var articleNum = $("#index_content .home_article").length,
-	columnNum = 1;
-	//check container width
-	function conWidthCheck(){
+	// check container width
+	var columnNum = 1;
+	function conWidCheck(){
 		var conWidth = $('#container').css('width').replace('px', '');
 		if (conWidth < 960) {
 			columnNum = 1;
+			posInit();
 		} else if (conWidth <= 1180) {
 			columnNum = 3;
+			articleName();
 		} else {
 			columnNum = 4;
+			articleName();
 		}
-
-		//console.log(columnNum);
-	};
-	conWidthCheck();
-	$(window).resize(function() {
-		conWidthCheck();
-		heightValue();
+	}
+	conWidCheck();
+	$(window).resize(function(){
+		conWidCheck();
 	});
 	// mark article name
 	function articleName(){
-		for (var i = 0; i < articleNum; i++) {
+		for (var i = 0; i < $("#index_content .home_article").length; i++) {
 			$("#index_content .home_article").eq(i).addClass("article_"+i);
 		}
+		posValue();
 	}
-	articleName();
-	// article name
-	var topArray = [],
-	leftArray = [0, 320, 640, 960]
-	function heightValue(){
-		for (var i = 0; i < articleNum; i++) {
-			if (i<columnNum) {
+	// article position on a labtop
+	function posValue(){
+		var topArray = [],
+		leftArray = [0, 320, 640, 960];
+		for (var i = 0; i < $("#index_content .home_article").length; i++) {
+			if (i < columnNum) {
 				$(".article_"+i).css("top", "0");
 				$(".article_"+i).css("left", leftArray[i]+"px");
 				topArray[i] = Number($(".article_"+i).css("height").replace("px", ""));
@@ -44,15 +43,14 @@ $(document).ready(function(){
 			}
 		}
 		var big = Math.max.apply(null, topArray);
-		$("#index_content").css("height", big);
+		$("#index_content").css("height", big+"px");
 	}
-	heightValue();
-	/*function contentHeight(){
-		var big = Math.max.apply(null, topArray);
-		if (columnNum > 1 ) {
-			$("#index_content").css("height", big);
-		} else {
-			$("#index_content").css("height", big);
+	// article position on a mobile
+	function posInit(){
+		for (var i = 0; i < $("#index_content .home_article").length; i++) {
+			$(".article_"+i).css("top", "");
+			$(".article_"+i).css("left", "");
 		}
-	}*/
+		$("#index_content").css("height", "");
+	}
 });
