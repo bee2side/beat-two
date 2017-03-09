0b69e2a565849edf1887f75f73ce21b6b1517a38
bee2side <bee2side@gmail.com>
2017-03-09 14:11:42 +0900

    1

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 				<span class="copyright">m.&nbsp;&nbsp;+82&nbsp;10&nbsp;9880&nbsp;1841<br>e.&nbsp;&nbsp;bee2side@gmail.com<br>&#169;<?php bloginfo('name'); ?></span>
 				<a href="<?php echo home_url(); ?>"><span class="b2btn"></span></a>
 			</div>
-		</div>
+		</div><!-- container -->
 		<?php wp_footer(); ?>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -8,16 +8,16 @@
 <title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/pageControl.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/indexControl.js"></script>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 		<div id="container">
-				<div id="header">
-					<div class="menu_normal">
-						<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-						<span class="menubtn"></span>
-					</div>
-					<div class="menu_open">
-					</div>
+			<div id="header">
+				<div class="menu_normal">
+					<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+					<span class="menubtn"></span>
 				</div>
+				<div class="menu_open">
+				</div>
+			</div>
